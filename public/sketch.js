83f467284907987c30f149397d6769fa186a231b
ncleves <nicholas@cleves.net>
2016-08-24 15:45:02 -0400

    Board improvements

--- public/sketch.js
+++ public/sketch.js
@@ -1,30 +1,56 @@
 /**
  * Created by Nix on 8/18/2016.
  */
-var height = 474;
-var width = 432;
+
 
 function setup (){
-    createCanvas(432, 474);
+    var width = 500;
+    var height = 550;
+
+    createCanvas(width, height);
     background(0, 158, 11);
     
     //Top side of board
     stroke(222, 184, 135);
     fill(222, 184, 135);
-    rect(0, 0, height, 30);
+    rect(0, 0, width, 75);
 
     //Left side of board
-    rect(0, 0, 30, height);
+    rect(0, 0, 50, height);
 
     //right side of board
-    rect(width-30, 0, 30, height);
+    rect(width-50, 0, 50, height);
 
     //bottom side of board
-    rect(0, height-30, width, 30);
+    rect(0, height-75, width, 75);
 
 }
 
 function draw (){
+    var width = 500;
+    var height = 550;
+    var spacing = 30;
+    var x = 0;
+    var y = 0;
+
+    // while (x < width-spacing){
+    //     stroke(0);
+    //     line(x, spacing, x, height-spacing);
+    //     x = x + spacing;
+    // }
+    //
+    // while (y < height-spacing){
+    //     stroke(0);
+    //     line(spacing, y, width-spacing, y);
+    //     y = y + spacing;
+    // }
+
+    // for(var x = 30; x <= width-30; x += 30){
+    //     for(var y = 30; x <= height-30; y += 30){
+    //         stroke(0);
+    //         line(x, y, width-x, height-y);
+    //     }
+    // }
 
 
 }
