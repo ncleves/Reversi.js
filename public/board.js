7caa6136e0728fdb82c93b31a9cd74ac3644ef97
ncleves <nicholas@cleves.net>
2016-09-23 19:39:43 -0400

    Reorganized the board object, fix errors

--- public/board.js
+++ public/board.js
@@ -2,49 +2,41 @@
  * Created by Nix on 9/18/2016.
  */
 
-var BOARD_ARRAY = createBoardArray();
+function Board(){
+    const ROWS = 8;
+    const COLS = 8;
 
-function createBoardArray(){
-    var ROWS = 8;
-    var COLS = 8;
+    this.createBoardArray = function(){
+        var boardArr = new Array(ROWS);
 
-    var boardArr = new Array(ROWS);
-
-    // make the cells array 8 blank arrays
-    for(var i = 0; i < ROWS; i++){
-        boardArr[i] = new Array(COLS);
-    }
-
-    // create the rest of the 2D array
-    for(var row = 0; row < ROWS; row++){
-        for(var col = 0; col < COLS; col++){
-            boardArr[row][col] = "_";
+        // make the cells array 8 blank arrays
+        for(var i = 0; i < ROWS; i++){
+            boardArr[i] = new Array(COLS);
         }
-    }
 
-    return boardArr;
-
-}
-
-var Board = {
-    board: BOARD_ARRAY,
-
-    getBoardCoor: function(row, col){
-        return this.board[row][col];
-    },
-
-    setBoardCoor: function(row, col, piece){
-        this.board[row][col] = piece;
-    },
-
-    returnClickCoor: function(coorX, coorY){
+        // create the rest of the 2D array
         for(var row = 0; row < ROWS; row++){
             for(var col = 0; col < COLS; col++){
-                if((coorY >= 50*(row+1) && coorY <= 50*(row+1)+50) && (coorX >= 50*(col+1) && coorX <= 50*(col+1)+50)){
-                    return [row, col];
+                if(row%2 && col%2){
+                    boardArr[row][col] = "O";
+                }else{
+                    boardArr[row][col] = "X";
                 }
+
             }
         }
-    }
 
-};
+        return boardArr;
+    };
+
+    this.boardArray = this.createBoardArray();
+
+    // this.getBoardCoor = function(row, col){
+    //     return this.board[row][col];
+    // };
+    //
+    // this.setBoardCoor = function(row, col, piece){
+    //     this.board[row][col] = piece;
+    // };
+
+}
