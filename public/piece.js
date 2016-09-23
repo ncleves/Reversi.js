370077e7c218382cf16af87e007ef96af7d6742f
ncleves <nicholas@cleves.net>
2016-09-23 19:41:07 -0400

    Added Piece.js + makePiece function :v:

---
+++ public/piece.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Nix on 8/23/2016.
+ */
+
+function Piece(){
+    this.makePiece = function(row, col, boardArray){
+
+        if(boardArray[row][col] == 'X'){
+            fill(255);
+            noStroke();
+            ellipse( ((col)*50)+75, ((row)*50)+75, 45, 45);
+        }
+
+        if(boardArray[row][col] == 'O'){
+            fill(0);
+            ellipse(((col)*50)+75, ((row)*50)+75, 45, 45);
+        }
+
+    }
+}
